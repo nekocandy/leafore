8dbb8c3365dd785c6e9b7e7e182006867b36bd1c
Piyush <bhangalepiyush@gmail.com>
2023-09-01 22:23:11 +0530

    feat(auth): add secret

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,6 +2,7 @@ import DiscordProvider from 'next-auth/providers/discord'
 import { NuxtAuthHandler } from '#auth'
 
 export default NuxtAuthHandler({
+  secret: useRuntimeConfig().auth.SECRET,
   providers: [
     // @ts-expect-error vite SSR issue - need to use .default
     DiscordProvider.default({
