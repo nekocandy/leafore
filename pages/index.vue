fcf9d6b090806ee0303214cb3493a608de148781
Piyush <bhangalepiyush@gmail.com>
2023-09-01 23:11:50 +0530

    chore(git): hook config

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+const { status, data, signIn, signOut } = useAuth()
 
+async function auth() {
+  if (status.value === 'authenticated')
+    signOut()
+
+  else
+    await signIn('discord')
+}
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center bg-zinc-900 font-mono text-white">
     Hello, Pizza!
+
+    <button bg-pink px-4 py-2 text-white @click="auth()">
+      {{ status === 'authenticated' ? 'Sign Out' : 'Sign In' }}
+    </button>
   </div>
 </template>
 
