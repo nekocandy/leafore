986a139ea4e93a4bc0b7aa49c69eed63587c8489
Piyush <bhangalepiyush@gmail.com>
2023-09-03 01:44:32 +0530

    feat: complete user card

--- components/Dashboard/Profile.vue
+++ components/Dashboard/Profile.vue
@@ -14,8 +14,8 @@ const userData = await $client.user.getUser.query({})
         <img class="ring-[#B5C4B2] -mb-3/4" h-24 rounded-full ring-10 :src="data?.user?.image || ''" alt="">
       </div>
     </div>
-    <div mt-18 px-4 py-4>
-      <div mb-4 h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+    <div mt-18 flex flex-col gap-4 px-4 py-4>
+      <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
 
       <div flex flex-col rounded-md bg-white>
         <div class="bg-[#D0D7DD]" rounded-t-md py-2 text-center text-sm>
@@ -26,6 +26,33 @@ const userData = await $client.user.getUser.query({})
           {{ numberpad(userData.points, 4) }}
         </div>
       </div>
+
+      <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+      <div flex items-center text-xs font-imprima>
+        <span>Total deeds completed</span>
+        <div ml-auto rounded bg-white px-2 py-1>
+          {{ numberpad(userData.points, 2) }}
+        </div>
+      </div>
+
+      <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+      <div flex items-center text-xs font-imprima>
+        <span>Total rewards earned</span>
+        <div ml-auto rounded bg-white px-2 py-1>
+          {{ numberpad(userData.points, 2) }}
+        </div>
+      </div>
+
+      <!-- <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+      <div flex items-center text-xs font-imprima>
+        <span>Total deeds completed</span>
+        <div ml-auto rounded bg-white px-2 py-1>
+          {{ userData.points }}
+        </div>
+      </div> -->
     </div>
   </div>
 </template>
