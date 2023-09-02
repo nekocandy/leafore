8abef0386a69c826b626c67a07730cb48f5dbec3
Piyush <bhangalepiyush@gmail.com>
2023-09-02 20:16:51 +0530

    feat(router/user): add getUser query

--- server/trpc/routers/user.ts
+++ server/trpc/routers/user.ts
@@ -1,7 +1,32 @@
-import { publicProcedure, router } from '../trpc'
+import { z } from 'zod'
+import { TRPCError } from '@trpc/server'
+import { protectedProcedure, publicProcedure, router } from '../trpc'
+import { prisma } from '~/lib/database'
 
 export const userRouter = router({
   hello: publicProcedure.query(({ ctx }) => {
     return ctx.user?.user?.name
   }),
+  getUser: protectedProcedure
+    .input(z.object({
+      email: z.string().email().optional(),
+    }))
+    .query(async ({ ctx, input }) => {
+      const email = input.email || ctx.user?.user?.email || ''
+
+      const userName = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userName) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'User not found',
+        })
+      }
+
+      return userName
+    }),
 })
