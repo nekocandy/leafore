a6e34700a007aa711880c8d9834f108b8790d23d
Piyush <bhangalepiyush@gmail.com>
2023-09-03 01:50:05 +0530

    feat: add quote banner

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -8,8 +8,12 @@
       <DashboardProfile />
     </div>
 
-    <div>
-      Yjere
+    <div col-span-9 h-full w-full>
+      <div grid grid-cols-2 h-full w-full>
+        <div class="bg-[#CDA2B1] text-[#533E45]" col-span-2 h-fit rounded-md py-4 text-center text-3xl font-k2d>
+          “Waste isn’t waste until we waste it”
+        </div>
+      </div>
     </div>
   </div>
 </template>
