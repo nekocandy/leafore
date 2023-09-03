ead2c0e800f8fc6d05d884dc6e6ad99036b42d57
Piyush <bhangalepiyush@gmail.com>
2023-09-03 13:25:52 +0530

    feat: claim rewards

---
+++ components/Rewards/Card.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { usePush } from 'notivue'
+
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  pointsRequired: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  semiDescription: {
+    type: String,
+    required: true,
+  },
+  currentPoints: {
+    type: Number,
+    required: true,
+  },
+})
+
+const { $client } = useNuxtApp()
+const push = usePush()
+
+const mutation = $client.rewards.redeemReward
+
+async function completeDeed() {
+  const promise = push.promise('Redeeming reward...')
+  await mutation.mutate({
+    id: props.id,
+    points: props.pointsRequired,
+  })
+
+  promise.resolve(
+    'Reward redeemed, further information would be emailed to you shortly! Thank you for your helping us to maintain a sustainable environment',
+  )
+}
+</script>
+
+<template>
+  <div flex flex-col gap-4 rounded-md bg-white px-4 py-4>
+    <div
+      h-40 rounded-md bg-cover bg-center bg-no-repeat
+      :style="{
+        backgroundImage: `url(${image})`,
+      }"
+    />
+
+    <div>
+      <p font-imprima class="text-xs text-[#94BAB8]" font-mono>
+        {{ semiDescription }}
+      </p>
+
+      <h1
+        font-imprima class="text-md text-[#425551]"
+      >
+        {{ title }}
+      </h1>
+    </div>
+
+    <button :disabled="$props.currentPoints < $props.pointsRequired" class="bg-[#94BAB2] disabled:cursor-not-allowed" justify-self-end rounded-md py-2 font-imprima text-white @click="completeDeed">
+      Redeem for {{ numberpad(pointsRequired, 2) }} Points
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/rewards.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+const userInfo = await $client.user.getUser.query({})
+const rewards = await $client.rewards.getRewards.query()
+</script>
+
+<template>
+  <div h-full w-full flex flex-col gap-2>
+    <h1 text-xl font-imprima class="text-[#645956]">
+      Rewards
+    </h1>
+
+    <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+    <div grid grid-cols-3 mt-4 gap-6 overflow-y-auto>
+      <RewardsCard
+        v-for="reward in rewards"
+        :id="reward.id"
+        :key="reward.id"
+        :title="reward.name"
+        :description="reward.description"
+        :semi-description="reward.semiDescription"
+        :points-required="reward.pointsRequired"
+        :current-points="userInfo.points"
+        :image="reward.image"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
