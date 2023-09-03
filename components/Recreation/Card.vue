4ce65b10a5ae2d4e1518e944272180e1d17999c9
Piyush <bhangalepiyush@gmail.com>
2023-09-03 14:49:25 +0530

    feat: recreational activities

---
+++ components/Recreation/Card.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { faker } from '@faker-js/faker'
+
+defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <div w-full flex gap-4 rounded-lg bg-white px-4 py-4>
+    <div
+      :style="{
+        backgroundImage: `url(${image})`,
+      }" class="h-full w-48 rounded-md bg-cover bg-center bg-no-repeat"
+    />
+
+    <div flex="~ col" w-full justify-between gap-4 pt-2>
+      <div>
+        <h1 font-imprima class="text-[#645956]">
+          {{ title }}
+        </h1>
+
+        <div flex items-center gap-2 text-xs class="text-[#B2B9C0]">
+          <span font-bold>Date: </span>
+          <span>{{ dayjs(faker.date.future()).format("DD/MM/YY") }}</span>
+        </div>
+
+        <div flex items-center gap-2 text-xs class="text-[#B2B9C0]">
+          <span font-bold>Time: </span>
+          <span>{{ dayjs(faker.date.future()).format("DD/MM/YY HH:MM") }}</span>
+        </div>
+
+        <div pt-2 text-xs>
+          {{ description }}
+        </div>
+      </div>
+
+      <div
+        self-end
+      >
+        <button
+          font-imprima
+          class="w-full rounded-md bg-[#DDD] px-12 py-2 font-imprima text-black"
+        >
+          Join
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/recreation.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import paper from '~/assets/recreation/paper.png'
+import doodle from '~/assets/recreation/doodle.png'
+import pot from '~/assets/recreation/pot.png'
+import clothes from '~/assets/recreation/clothes.png'
+
+const DATA = [
+  {
+    image: paper,
+    title: 'Recycle Paper',
+    description: 'Recycle paper to save trees',
+  },
+  {
+    image: doodle,
+    title: 'Doodle',
+    description: 'Doodle to express your creativity',
+  },
+  {
+    image: pot,
+    title: 'Pot a Plant',
+    description: 'Pot a plant to make your home greener',
+  },
+  {
+    image: clothes,
+    title: 'Donate Clothes',
+    description: 'Donate clothes to the needy',
+  },
+]
+</script>
+
+<template>
+  <div h-full w-full flex flex-col gap-2>
+    <h1 text-xl font-imprima class="text-[#645956]">
+      Recreational Activity
+    </h1>
+
+    <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+    <div grid grid-cols-2 mt-4 gap-6 overflow-y-auto>
+      <RecreationCard
+        v-for="data in DATA"
+        :key="data.title"
+        :image="data.image"
+        :title="data.title"
+        :description="data.description"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
