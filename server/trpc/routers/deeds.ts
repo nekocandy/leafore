3cf40f666d2f0f3554875d740bc23bce4fb18c08
Piyush <bhangalepiyush@gmail.com>
2023-09-03 11:33:52 +0530

    feat: complete deed

--- server/trpc/routers/deeds.ts
+++ server/trpc/routers/deeds.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+import { nanoid } from 'nanoid'
 import { protectedProcedure, router } from '../trpc'
 import { prisma } from '~/lib/database'
 
@@ -6,5 +8,39 @@ export const deedsRouter = router({
     .query(async () => {
       const deeds = await prisma.deeds.findMany()
       return deeds
+    }),
+
+  completeDeed: protectedProcedure
+    .input(z.object({
+      id: z.string(),
+      points: z.number(),
+    }))
+    .mutation(async ({ input, ctx }) => {
+      const userId = await prisma.user.findUnique({
+        where: {
+          email: ctx.user.user?.email || '',
+        },
+      })
+      const deed = await prisma.deedsCompleted.create({
+        data: {
+          id: nanoid(),
+          deedId: input.id,
+          userId: userId!.id,
+          points: input.points,
+        },
+      })
+
+      await prisma.user.update({
+        where: {
+          id: userId!.id,
+        },
+        data: {
+          points: {
+            increment: input.points,
+          },
+        },
+      })
+
+      return deed
     }),
 })
