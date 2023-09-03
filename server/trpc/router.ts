3b911c83b01da9fd153156b14225912eeaa0c29a
Piyush <bhangalepiyush@gmail.com>
2023-09-03 10:16:19 +0530

    feat(router): get deeds router

--- server/trpc/router.ts
+++ server/trpc/router.ts
@@ -1,7 +1,9 @@
 import { router } from './trpc'
+import { deedsRouter } from '~/server/trpc/routers/deeds'
 import { userRouter } from '~/server/trpc/routers/user'
 
 export const appRouter = router({
   user: userRouter,
+  deeds: deedsRouter,
 }) // export type definition of API
 export type AppRouter = typeof appRouter
---
+++ server/trpc/routers/deeds.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { TRPCError } from '@trpc/server'
+import { protectedProcedure, publicProcedure, router } from '../trpc'
+import { prisma } from '~/lib/database'
+
+export const deedsRouter = router({
+  getDeeds: protectedProcedure
+    .query(async ({ ctx }) => {
+      return [
+        {
+          id: 1,
+          image: 'https://picsum.photos/200',
+          title: 'Deed 1',
+          pointsRequired: 10,
+        },
+      ]
+    }),
+})
