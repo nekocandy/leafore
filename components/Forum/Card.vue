ec19db9e308d85dff73d68d6edfa4d63fdbca57a
Piyush <bhangalepiyush@gmail.com>
2023-09-03 14:07:20 +0530

    feat: forum page

---
+++ components/Forum/Card.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div flex flex-col justify-between gap-4 rounded-md bg-white px-4 py-4 font-imprima>
+    <div flex items-center gap-1 text-xs font-mono class="-mb-2">
+      <div>
+        Post ID:
+      </div>
+      <div class="text-[#645956]" underline>
+        {{ id }}
+      </div>
+    </div>
+    <h1 font-imprima class="text-[#645956]">
+      {{ title }}
+    </h1>
+
+    <div flex items-center gap-1 text-sm>
+      <span class="text-[#645956]">Post By</span>
+      <span class="font-bold text-[#645956]">{{ userName }}</span>
+    </div>
+
+    <button class="bg-[#718B86]" w-full flex items-center justify-center gap-1 rounded-md px-8 py-2 text-white>
+      <div i-tabler-heart />
+      <div>
+        Contribute
+      </div>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/forum.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+const posts = await $client.forum.getPosts.query()
+</script>
+
+<template>
+  <div h-full w-full flex flex-col gap-2>
+    <h1 text-xl font-imprima class="text-[#645956]">
+      Forum
+    </h1>
+
+    <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+    <div grid grid-cols-3 mt-4 gap-6 overflow-y-auto>
+      <ForumCard
+        v-for="post in posts"
+        :id="post.id"
+        :key="post.id"
+        :title="post.title"
+        :user-name="post.user.name || 'Anonymous'"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
