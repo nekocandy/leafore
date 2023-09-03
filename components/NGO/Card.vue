124b5e902eef290d84e32b4f887899cb94ec5026
Piyush <bhangalepiyush@gmail.com>
2023-09-03 15:09:13 +0530

    feat: add NGOs page

---
+++ components/NGO/Card.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  location: String,
+  type: String,
+  contact: String,
+})
+</script>
+
+<template>
+  <div flex flex-col gap-2 rounded-md bg-white px-4 py-2 font-sans class="text-[#545F71]">
+    <h1 font-extrabold>
+      {{ name }}
+    </h1>
+    <div flex items-center gap-1 text-xs>
+      <div i-tabler-location />
+      {{ location }}
+    </div>
+
+    <div flex items-center gap-1 text-xs>
+      <div i-tabler-info-circle />
+      {{ type }}
+    </div>
+
+    <div w-full px-2>
+      <button class="bg-[#718B86]" w-full flex items-center justify-center gap-2 rounded-md py-2 font-bold text-white>
+        <div i-tabler-phone />
+        <div>
+          Contact:
+        </div>
+        {{ contact }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/ngo.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import NGOImage from '~/assets/ngo/banner.png'
+
+const NGOS = [
+  {
+    name: 'Adsons Foundation',
+    location: 'India',
+    type: 'Non-governmental organization',
+    contact: '09922339768',
+  },
+
+  {
+    name: 'Love Heals Foundation',
+    location: 'India',
+    type: 'Non-profit organization',
+    contact: '09922339768',
+  },
+]
+</script>
+
+<template>
+  <div h-full w-full flex flex-col gap-2>
+    <h1 text-xl font-imprima class="text-[#645956]">
+      Support a cause
+    </h1>
+
+    <div h-0.5 w-full rounded-full class="bg-[#8F7264]" />
+
+    <div
+
+      :style="{
+        backgroundImage: `url(${NGOImage})`,
+      }"
+      class="text-[#533E45]"
+      h-48 flex items-center rounded-lg bg-cover bg-no-repeat px-8 text-center text-2xl font-imprima
+    >
+      NGO's <br>  Nearby
+    </div>
+
+    <div grid grid-cols-2 mt-4 gap-6 overflow-y-auto>
+      <NGOCard
+        v-for="ngo in NGOS"
+        :key="ngo.name"
+        :name="ngo.name"
+        :location="ngo.location"
+        :type="ngo.type"
+        :contact="ngo.contact"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
